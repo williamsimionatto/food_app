e98f0703978e7fcaef0ffefff0e07c31adbc89c0
William Simionatto <william.sn0226@gmail.com>
2023-05-02 20:30:33 -0300

    chore: change MaterialApp to GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:delivery_food_app/ui/pages/pages.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
