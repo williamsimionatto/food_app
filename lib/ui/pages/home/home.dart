8dd203436b6c7fbf5c6fac553fe63af38afcbec1
William Simionatto <william.sn0226@gmail.com>
2023-05-02 17:48:11 -0300

    feat: initialize MainFoodPage

---
+++ lib/ui/pages/home/home.dart
@@ -0,0 +1,1 @@
+export './main_food_page.dart';
---
+++ lib/ui/pages/home/main_food_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:delivery_food_app/ui/utils/colors.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 15),
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: const <Widget>[
+                      Text('Country'),
+                      Text('City'),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                      child: const Icon(Icons.search, color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
